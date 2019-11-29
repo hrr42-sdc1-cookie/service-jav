adfde68d23057315d99f68dc1f63fc53b0be3b6c
Miaozhen Zhang <miaozhenzhang666@gmail.com>
2019-11-29 12:38:16 -0600

    Revert to the initial commit

--- server/db/index.js
+++
@@ -1,6 +0,0 @@
-const mongoose = require('mongoose');
-const mongoUri = 'mongodb://127.0.0.1/overview';
-
-const db = mongoose.connect(mongoUri, {useNewUrlParser: true, useUnifiedTopology: true});
-
-module.exports = db;
