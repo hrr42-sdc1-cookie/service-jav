53ed80bee9fa736da94c835b30798fc9d79f08b3
Miaozhen Zhang <miaozhenzhang666@gmail.com>
2019-12-07 16:50:14 -0600

    Adjust Title styling

--- client/src/components/title.jsx
+++ client/src/components/title.jsx
@@ -6,6 +6,11 @@ const titleStyle = {
   // Use this for now
   fontFamily: '"Trebuchet MS", Helvetica, sans-serif',
   fontSize: '24px',
+  margin: '0px 0px 15px 0px',
+  padding: '0px 0px 30px 0px',
+  borderBottomStyle: 'solid',
+  borderColor: 'grey',
+  borderWidth: '0.8px',
 };
 
 const Title = (props) => (
