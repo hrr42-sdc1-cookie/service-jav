358157721efa9ae44e92900dbab2c3769f9f0e76
Miaozhen Zhang <miaozhenzhang666@gmail.com>
2019-12-11 14:40:21 -0600

    Change the script name and the name of the css file

--- server/app.js
+++ server/app.js
@@ -1,14 +1,21 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 
 const overviewDAO = require('./db/overviewDAO.js');
 
 const app = express();
 
+app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
 app.use(express.static(__dirname + '/../client/dist'));
+
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  next();
+});
 
 app.get('/api/restaurant/:restaurantId', (req, res) => {
   let id = req.params.restaurantId;
